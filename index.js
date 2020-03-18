02189e2f24b3c37754717c5dee78c16dab062043
Espen Bakken <espenbakken@Espens-MacBook-Air.local>
2020-03-18 19:36:09 +0100

    Endret oppsettet for å lettere kunne legge til bildeopplasting, samt lagt til noen nye funksjoner i index.js

--- index.js
+++ index.js
@@ -1,39 +1,66 @@
 
-function renderList (){
-    const outputList = JSON.parse(window.localStorage.getItem("outputList")) || [];
+function renderMembers (){
+    const outputMembers = JSON.parse(window.localStorage.getItem("outputMembers")) || [];
 
-    const outputListTwo = document.getElementById("outputList");
-    outputListTwo.innerHTML = "";
-    for(const product of outputList){
+    const outputMembersEl = document.getElementById("outputMembers");
+    outputMembersEl.innerHTML = "";
+    for(const product of outputMembers){
         const productTwo = document.createElement("div");
-        const {member, task, description} = product;
+        const {member} = product;
         
-        productTwo.innerHTML = "<h4>" + member + "</h4>" + 
-            "<div>Arbeidsoppgave: " + task + "</div>" +  
-            "<div>Arbeidsbeskrivelse: " + description + "</div>";
-        outputListTwo.appendChild(productTwo);
+        productTwo.innerHTML = "<h4>" + member + "</h4>" ;
+        outputMembersEl.appendChild(productTwo);
     }
 }
 
-function createNew(event){
+function createNewMember(event){
     event.preventDefault();
 
     const member = document.getElementById("member-input").value;
-    const task = document.getElementById("task-input").value;
+    /*const task = document.getElementById("task-input").value;
     const description = document.querySelector("[name='description']").value;
+*/
+    const product = {member};
 
-    const product = {member, task, description};
-
-    const outputList = JSON.parse(window.localStorage.getItem("outputList")) || [];
-    outputList.push(product);
-    window.localStorage.setItem("outputList", JSON.stringify(outputList)); 
-    renderList();
+    const outputMembers = JSON.parse(window.localStorage.getItem("outputMembers")) || [];
+    outputMembers.push(product);
+    window.localStorage.setItem("outputMembers", JSON.stringify(outputMembers)); 
+    renderMembers();
 
     event.target.reset();
 }
 
-window.addEventListener("storage", function(event){
+/*window.addEventListener("storage", function(event){
     if (event.key === "outputlist"){
         renderList();
     }
-});
+});*/
+
+function renderTasks(){
+    const outputTask = JSON.parse(window.localStorage.getItem("outputTask")) || [];
+
+    const outputTaskEl = document.getElementById("outputTasks");
+    outputTaskEl.innerHTML = "";
+    for(const product of outputTask){
+        const productTwo = document.createElement("div");
+        const {task} = product;
+        
+        productTwo.innerHTML = "<h4>" + task + "</h4>" ;
+        outputTaskEl.appendChild(productTwo);
+    }
+}
+
+function createNewTask(event){
+    event.preventDefault();
+
+    const task = document.querySelector("[name='task']").value;
+
+    const product = {task};
+
+    const outputTask = JSON.parse(window.localStorage.getItem("outputTask")) || [];
+    outputTask.push(product);
+    window.localStorage.setItem("outputTask", JSON.stringify(outputTask)); 
+    renderTasks();
+
+    event.target.reset();
+}
