692e6fabf5f0d5aad96b45ca34d3f7c613436ae0
bilalhaider1 <56349952+bilalhaider1@users.noreply.github.com>
2020-03-22 16:15:51 +0100

    Update index.js
    Lagt til bildefunksjon

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function renderList (){
     const outputList = JSON.parse(window.localStorage.getItem("outputList")) || [];
 
@@ -6,23 +5,23 @@ function renderList (){
     outputListTwo.innerHTML = "";
     for(const product of outputList){
         const productTwo = document.createElement("div");
-        const {member, task, description} = product;
+        const {member, task, description, image} = product;
         
-        productTwo.innerHTML = "<h4>" + member + "</h4>" + 
-            "<div>Arbeidsoppgave: " + task + "</div>" +  
-            "<div>Arbeidsbeskrivelse: " + description + "</div>";
+       productTwo.innerHTML = "<h4>" + member + "</h4>" +  "<div>Arbeidsoppgave: " + task + "</div>" +  
+            "<div>Arbeidsbeskrivelse: " + description + "</div>" + "<div><img src='" + image + "' /></div>";
         outputListTwo.appendChild(productTwo);
     }
 }
-
+      
 function createNew(event){
     event.preventDefault();
 
     const member = document.getElementById("member-input").value;
     const task = document.getElementById("task-input").value;
     const description = document.querySelector("[name='description']").value;
-
-    const product = {member, task, description};
+    const image = document.querySelector("[name='image']").files[0].name;
+  
+    const product = {member, task, description, image};
 
     const outputList = JSON.parse(window.localStorage.getItem("outputList")) || [];
     outputList.push(product);
